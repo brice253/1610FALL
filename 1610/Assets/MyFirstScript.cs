a3c7396e46c28e036f5f92e3865a8c494b50c94a
Brad Rice <rice_bowl00@hotmail.com>
2018-09-11 15:27:28 -0600

    Variables and Functions
    Watched 2nd unity tutorial video

--- 1610/Assets/MyFirstScript.cs
+++ 1610/Assets/MyFirstScript.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class MyFirstScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    int myInt = 5;
+
+
+    void Start()
+    {
+        myInt = MultiplyByTwo(myInt);
+        Debug.Log(myInt);
+    }
+
+
+    int MultiplyByTwo(int number)
+    {
+        int ret;
+        ret = number * 2;
+        return ret;
+    }
 }
