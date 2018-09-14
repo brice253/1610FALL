8fd850e95800a2bae3933fd26da190953a23a8dc
Brad Rice <rice_bowl00@hotmail.com>
2018-09-14 13:59:44 -0600

    Start and Awake Functions

---
+++ 1610/Assets/StartAndAwake.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartAndAwake : MonoBehaviour 
+{
+    void Awake()
+    {
+        Debug.Log("Awake called.");
+    }
+
+
+    void Start()
+    {
+        Debug.Log("Start called.");
+    }
+}
