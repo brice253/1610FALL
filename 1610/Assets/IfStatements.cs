f6fcea38fba813db1948033ac401b27e2e894278
Brad Rice <rice_bowl00@hotmail.com>
2018-09-15 12:21:13 -0600

    If and Else Statements
    watched tutorial on discord and practice if and else statements

---
+++ 1610/Assets/IfStatements.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfStatements : MonoBehaviour 
+{
+    public int A;
+    public int B;
+    public int C;
+
+    void Update()
+    {
+        if (A + B == C)
+        {
+            print("Correct");
+        }
+        else
+        {
+            print("Incorrect");
+        }
+    }	
+}
