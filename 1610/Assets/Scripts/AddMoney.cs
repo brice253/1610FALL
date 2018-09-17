0997684795232cd5da42f603fcce2a4ed7f85867
Brad Rice <rice_bowl00@hotmail.com>
2018-09-17 15:46:49 -0600

    Add Money Button

---
+++ 1610/Assets/Scripts/AddMoney.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddMoney : MonoBehaviour
+{
+
+    public int Money = 10;
+
+    public void AddMoreMoney(int money)
+    {
+        Money += money;
+    }
+}
