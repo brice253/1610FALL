5815a65d07c0d6b0ae80b7d051ff6153998a80e9
Brad Rice <rice_bowl00@hotmail.com>
2018-09-18 15:08:42 -0600

    Enum and Switch practice

--- 1610/Assets/Practice Scripts/Movements.cs
+++ 1610/Assets/Practice Scripts/Movements.cs
@@ -2,15 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movements : MonoBehaviour {
+public class Movements : MonoBehaviour { 
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
+    public enum MovementType
+    {
+        MoveX,
+        MoveY,
+        MoveZ
+    }
+
+    public MovementType CurrentMovementType;
+
+    public Vector3 Movement;
+    public float Speed;
+
 	// Update is called once per frame
-	void Update () {
-		
+	void Update () 
+    {
+        switch (CurrentMovementType)
+        {
+            case MovementType.MoveX:
+                Movement.x = Speed;
+                break;
+            case MovementType.MoveY:
+                Movement.y = Speed;
+                break;
+            case MovementType.MoveZ:
+                Movement.z = Speed;
+                break;
+        }
+        transform.Rotate(Movement * Time.deltaTime);
 	}
 }
