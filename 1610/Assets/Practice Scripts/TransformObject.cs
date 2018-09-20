b68bb2d052910e97eb3f58faa55119e9c32e6655
Brad Rice <rice_bowl00@hotmail.com>
2018-09-20 14:09:01 -0600

    Practiced transforms

---
+++ 1610/Assets/Practice Scripts/TransformObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformObject : MonoBehaviour {
+
+    public Vector3 Movement;
+	
+
+	// Update is called once per frame
+	void Update () 
+    {
+        transform.Translate(Movement);
+        //transform.Rotate(Movement);
+	}
+}
