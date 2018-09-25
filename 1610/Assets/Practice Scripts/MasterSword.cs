5153c39537c810df216eef83f3ebbd3cd10a2006
Brad Rice <rice_bowl00@hotmail.com>
2018-09-25 16:23:37 -0600

    Practiced working with variables

---
+++ 1610/Assets/Practice Scripts/MasterSword.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MasterSword : MonoBehaviour 
+{
+    public string Name = "Master Sword";
+    public string Owner = "Link";
+
+    public int Damage = -75;
+
+}
