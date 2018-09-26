8c30aa051a34a6a7ee07612f58792d9f2b7a9dd3
Brad Rice <rice_bowl00@hotmail.com>
2018-09-26 13:10:57 -0600

    More Character Controller practice

--- 1610/Assets/Scripts/Character/CharacterMover.cs
+++ 1610/Assets/Scripts/Character/CharacterMover.cs
@@ -4,13 +4,20 @@ public class CharacterMover : MonoBehaviour
 {
 
     public CharacterController Controller;
+    private Vector3 position;
+    public float Speed = 3.0f;
 
-	void Start () {
+	void Start () 
+    {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update () 
+    {
+        position.x = Speed * Input.GetAxis("Horizontal");
+        position.y = Speed * Input.GetAxis("Vertical");
+        Controller.Move(position*Time.deltaTime);
+
 	}
 }
