9a89f790953585fa5bc11a106457fd8882f83103
Brad Rice <rice_bowl00@hotmail.com>
2018-09-26 12:55:45 -0600

    Discord Character Controller tutorial practice

---
+++ 1610/Assets/Scripts/Character/CharacterMover.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CharacterMover : MonoBehaviour 
+{
+
+    public CharacterController Controller;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
