53492271874c48e9322ed31e16cf6c3b31fcc01d
Brad Rice <rice_bowl00@hotmail.com>
2018-09-27 15:32:32 -0600

    Created object that subtracts from player health when triggered

---
+++ 1610/Assets/Scripts/Triggers/Ouch.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class Ouch : ScriptableObject
+{
+
+    public FloatData Data;
+
+    public void OnOuch(FloatData outsideData)
+    {
+        outsideData.Value -= Data.Value;
+    }
+
+}
