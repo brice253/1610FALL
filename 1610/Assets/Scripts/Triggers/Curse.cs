a6f2b01396235fa8c56dc153f3cb063767497a0c
Brad Rice <rice_bowl00@hotmail.com>
2018-09-28 13:02:44 -0600

    Created object that divides Player health by 10 when triggered

---
+++ 1610/Assets/Scripts/Triggers/Curse.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class Curse : ScriptableObject
+{
+
+    public FloatData Data;
+
+    public void OnCurse(FloatData outsideData)
+    {
+        outsideData.Value /= Data.Value;
+    }
+
+}
