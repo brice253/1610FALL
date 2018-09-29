444906b3b2478c8b91d294b9383378b33db2b5c3
Brad Rice <rice_bowl00@hotmail.com>
2018-09-29 15:06:24 -0600

    practiced writing variables and functions to understand them better

---
+++ 1610/Assets/Practice/Practice Scripts/VariablesAndFunctions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariablesAndFunctions : MonoBehaviour {
+    string myName = "Brad";
+
+    private void Start()
+    {
+        myName = FullName("Rice");
+        Debug.Log(myName);
+    }
+
+    string FullName(string last)
+    {
+        string lastName;
+
+        lastName = myName + last;
+
+        return lastName;
+    }
+
+
+}
