18f5ee965815d29ee2832cf21e6586efa7d2e4dd
Brad Rice <rice_bowl00@hotmail.com>
2018-10-01 13:16:51 -0600

    practiced While Loops

---
+++ 1610/Assets/Practice/Practice Scripts/WhileLoop.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhileLoop : MonoBehaviour
+{
+    float money = 0.00f;
+
+
+    void Start()
+    {
+        while (money > 0)
+        {
+            Debug.Log("I've got "+money+"!");
+            money--;
+        }
+    }
+}
