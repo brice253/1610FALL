7f9b4beb18a82eccd3ff1bd3a6b23cc073e34b39
Brad Rice <rice_bowl00@hotmail.com>
2018-10-08 15:20:24 -0600

    Class lecture (instancing and destroying objects)

---
+++ 1610/Assets/Scripts/DestroyObj.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObj : MonoBehaviour 
+{
+
+	// Use this for initialization
+    IEnumerator Start () 
+    {
+        yield return new WaitForSeconds(2);
+        Destroy(gameObject);
+	}
+
+    private void OnCollisionEnter(Collision other)
+    {
+        Destroy(gameObject);
+        Destroy(other.gameObject);
+    }
+
+}
---
+++ 1610/Assets/Scripts/InstanceObject.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InstanceObject : MonoBehaviour 
+{
+
+    public GameObject Instance;
+
+    void Update () 
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Instantiate(Instance, transform.position, transform.rotation);
+
+        }
+	}
+}
