c31063f75fe0645158d212d8098300fb89a1b9a1
Brad Rice <rice_bowl00@hotmail.com>
2018-10-09 15:01:38 -0600

    Learned how to instance multiple objects from discord tutorial

---
+++ 1610/Assets/Scripts/InstancingBase.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class InstancingBase : ScriptableObject
+{
+
+	private Vector3 position;
+
+	public void InstanceObject (GameObject prefab)
+	{
+		position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		position.z = 0;
+		Instantiate(prefab, position, Quaternion.identity);
+	}
+
+}
