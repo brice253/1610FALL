75f0143d68db0e7df58ca892693eb2dcc9330628
Brad Rice <rice_bowl00@hotmail.com>
2018-10-11 16:05:22 -0600

    EqualityOperator script can tell if two values are equal or not

--- 1610/Assets/Practice/Practice Scripts/EqualityOperators.cs
+++ 1610/Assets/Practice/Practice Scripts/EqualityOperators.cs
@@ -2,15 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EqualityOperators : MonoBehaviour {
+public class EqualityOperators : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
+	public int Apples;
+	public int Bananas;
+
+	private void Update()
+	{
+		if (Apples == Bananas)
+		{
+			print("Same amount of apples and bananas");
+		}
+
+		if (Apples > Bananas)
+		{
+			print("You have more apples");
+		}
+
+		if (Apples < Bananas)
+		{
+			print("you have more bananas");
+		}
 		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+		if (Apples != Bananas)
+		{
+			print("The amount of apples and bananas is not equal");
+		}
 		
 	}
 }
