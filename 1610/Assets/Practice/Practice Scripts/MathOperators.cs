9b59d3606932da7f54caf1a470bf22f4fb8d4b03
Brad Rice <rice_bowl00@hotmail.com>
2018-10-13 14:48:19 -0600

    Practiced writing different math operations

--- 1610/Assets/Practice/Practice Scripts/MathOperators.cs
+++ 1610/Assets/Practice/Practice Scripts/MathOperators.cs
@@ -2,15 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MathOperators : MonoBehaviour {
+public class MathOperators : MonoBehaviour
+{
+
+	public float A;
+	public float B;
+	public float C;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start()
+	{
+		//C = A + B;
+		//C = A - B;
+		//C++; // "++" means add one to whatever C is
+		C--; // "--" means subtract one from whatever C is
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+
 }
