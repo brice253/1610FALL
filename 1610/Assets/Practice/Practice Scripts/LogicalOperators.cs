1bfd10ff698eb28170a93266ba464f1f05518c1e
Brad Rice <rice_bowl00@hotmail.com>
2018-10-13 14:32:25 -0600

    Practiced writing logical operators in scripts

--- 1610/Assets/Practice/Practice Scripts/LogicalOperators.cs
+++ 1610/Assets/Practice/Practice Scripts/LogicalOperators.cs
@@ -2,15 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LogicalOperators : MonoBehaviour {
+public class LogicalOperators : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
+	public bool FoodReady;
+	public bool LightOn;
+
+
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update () 
+	{
+		if (FoodReady && LightOn) // "&&" means both have to be true
+		{
+			print("Food is ready and light is on.");
+		}
+
+		if (FoodReady || LightOn) // "||" means only one needs to be true
+		{
+			print("Either food is ready or light is on.");
+		}
+ 	}
 }
