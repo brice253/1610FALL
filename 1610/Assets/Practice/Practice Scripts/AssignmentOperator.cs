486696d02ad756f700a08ec2921e5236acace96c
Brad Rice <rice_bowl00@hotmail.com>
2018-10-23 13:40:40 -0600

    Learned how to write an Assignment Operator in script

--- 1610/Assets/Practice/Practice Scripts/AssignmentOperator.cs
+++ 1610/Assets/Practice/Practice Scripts/AssignmentOperator.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AssignmentOperator : MonoBehaviour {
+public class AssignmentOperator : MonoBehaviour
+{
 
+	public SpriteRenderer Sprite;
+	
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+		Sprite = GetComponent<SpriteRenderer>();
 	}
 	
 	// Update is called once per frame
