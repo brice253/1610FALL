46cdd44905df5924b21f3e8b65b48da652149eff
Brad Rice <rice_bowl00@hotmail.com>
2018-10-24 14:40:42 -0600

    Created new script

---
+++ 1610/Assets/Scripts/Loops.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Loops : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print("Word 0");
+		print("Word 1");
+		print("Word 2");
+		print("Word 3");
+		print("Word 4");
+		print("Word 5");
+		print("Word 6");
+		print("Word 7");
+		print("Word 8");
+		print("Word 9");
+		print("Word 10");
+		print("Word 11");
+		print("Word 12");
+		print("Word 13");
+		print("Word 14");
+		print("Word 15");
+		print("Word 16");
+		print("Word 17");
+		print("Word 18");
+		print("Word 19");
+		print("Word 20");
+		print("Word 21");
+		print("Word 22");
+		print("Word 23");
+		print("Word 24");
+		print("Word 25");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
