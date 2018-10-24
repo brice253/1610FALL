ba45eb140df4dcc2ee40ffb6dd65735087b7e47b
Brad Rice <rice_bowl00@hotmail.com>
2018-10-24 15:05:37 -0600

    Class lecture

--- 1610/Assets/Scripts/Loops.cs
+++ 1610/Assets/Scripts/Loops.cs
@@ -3,36 +3,23 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public class Loops : MonoBehaviour {
+public class Loops : MonoBehaviour
+{
+
+	public string[] Names;
+	public GameObject[] Things;
 
 	// Use this for initialization
 	void Start () {
-		print("Word 0");
-		print("Word 1");
-		print("Word 2");
-		print("Word 3");
-		print("Word 4");
-		print("Word 5");
-		print("Word 6");
-		print("Word 7");
-		print("Word 8");
-		print("Word 9");
-		print("Word 10");
-		print("Word 11");
-		print("Word 12");
-		print("Word 13");
-		print("Word 14");
-		print("Word 15");
-		print("Word 16");
-		print("Word 17");
-		print("Word 18");
-		print("Word 19");
-		print("Word 20");
-		print("Word 21");
-		print("Word 22");
-		print("Word 23");
-		print("Word 24");
-		print("Word 25");
+		for (int i = 0; i < Names.Length; i++)
+		{
+			print(Names[i]);
+		}
+
+		foreach (var thing in Things)
+		{
+			print(thing);
+		}
 	}
 	
 	// Update is called once per frame
