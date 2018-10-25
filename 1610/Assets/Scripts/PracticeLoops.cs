8724ad212d69fe1f03f436f50384e11a2576c20d
Brad Rice <rice_bowl00@hotmail.com>
2018-10-25 14:19:23 -0600

    Practiced loops and writing lists

--- 1610/Assets/Scripts/PracticeLoops.cs
+++ 1610/Assets/Scripts/PracticeLoops.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PracticeLoops : MonoBehaviour {
+public class PracticeLoops : MonoBehaviour
+{
+
+	public string[] Food;
 
 	// Use this for initialization
 	void Start () {
-		
+		for (int i = 0; i < Food.Length; i++)
+		{
+			print(Food[i]);
+		}
 	}
 	
 	// Update is called once per frame
