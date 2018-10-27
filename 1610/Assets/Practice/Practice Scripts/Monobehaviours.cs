6cd05e327262847259268dd3c9cf43cc2b1b184f
Brad Rice <rice_bowl00@hotmail.com>
2018-10-27 12:28:30 -0600

    Created new scene called Behaviours

--- 1610/Assets/Practice/Practice Scripts/Monobehaviours.cs
+++ 1610/Assets/Practice/Practice Scripts/Monobehaviours.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Monobehaviours : MonoBehaviour {
+	private void Awake()
+	{
+		print("Awake");
+	}
 
 	// Use this for initialization
 	void Start () {
+		print("something");
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		print("Update");
 	}
 }
