0779c8b57ef7064970cf048c52617409d9d76400
Brad Rice <rice_bowl00@hotmail.com>
2018-10-30 13:41:16 -0600

    Weapons Enums
    script prints what weapon player is using

---
+++ 1610/Assets/Scripts/SwitchStatement2.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using TMPro;
+using UnityEngine;
+
+public class SwitchStatement2 : MonoBehaviour
+{
+
+	public GameWeapons.Weapons GameWeapon;
+
+	void Update () {
+		switch (GameWeapon)
+		{
+				case GameWeapons.Weapons.Sword:
+					print("Player is using a sword");
+					break;
+				
+				case GameWeapons.Weapons.Axe:
+					print("Player is using a axe");
+					break;
+				
+				case GameWeapons.Weapons.Bow:
+					print("Player is using a bow");
+					break;
+				
+				case GameWeapons.Weapons.Spear:
+					print("Player is using a spear");
+					break;
+				
+				case GameWeapons.Weapons.Shoe:
+					print("Player is using a shoe");
+					break;
+					
+		}
+	}
+}
