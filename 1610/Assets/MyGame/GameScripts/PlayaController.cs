e0a8f53e478a54687527b5e215436f51041037b8
Brad Rice <rice_bowl00@hotmail.com>
2018-11-17 11:19:33 -0700

    Worked on game

--- 1610/Assets/MyGame/GameScripts/PlayaController.cs
+++ 1610/Assets/MyGame/GameScripts/PlayaController.cs
@@ -1,16 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Serialization;
 using UnityEngine;
 
-public class PlayaController : MonoBehaviour {
+public class PlayaController : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private Rigidbody2D rb2d;
+
+    public float Speed;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHoriz = Input.GetAxis("Horizontal");
+        Vector2 move = new Vector2(moveHoriz, 0);
+        rb2d.AddForce(move * Speed);
+    }
 }
