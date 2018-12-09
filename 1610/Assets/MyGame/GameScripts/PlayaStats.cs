0c4e02ad57aa3d4522982986b51682fdaa998372
Brad Rice <rice_bowl00@hotmail.com>
2018-12-09 11:11:56 -0700

    Game has if and else statements

--- 1610/Assets/MyGame/GameScripts/PlayaStats.cs
+++ 1610/Assets/MyGame/GameScripts/PlayaStats.cs
@@ -12,5 +12,9 @@ public class PlayaStats : MonoBehaviour
         {
             Debug.Log("DEAD");
         }
+        else
+        {
+            Debug.Log("Player is healthy");
+        }
     }
 }
