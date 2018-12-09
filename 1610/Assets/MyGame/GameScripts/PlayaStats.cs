f517e5febc76a338e1925e7aa7727ca9c181944f
Brad Rice <rice_bowl00@hotmail.com>
2018-12-09 10:58:28 -0700

    Created lists, added health powerup, if players health zero print "DEAD"

---
+++ 1610/Assets/MyGame/GameScripts/PlayaStats.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayaStats : MonoBehaviour
+{
+    public FloatData PlayerHealth;
+
+    private void Update()
+    {
+        if (PlayerHealth.Value <= 0)
+        {
+            Debug.Log("DEAD");
+        }
+    }
+}
